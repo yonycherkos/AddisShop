2341fd8ceecfebf336cd4f6073c5a9f5054b3d48
yonatan <yonycherkos@gmail.com>
2023-05-30 20:34:29 +0300

    002 React-Bootstrap Setup, Header & Footer Components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,17 @@
+import { Container } from 'react-bootstrap';
+import Footer from './components/Footer';
+import Header from './components/Header';
+
 function App() {
   return (
-    <div className='App'>
-      <p>Welcome to AddisShop</p>
+    <div>
+      <Header />
+      <main>
+        <Container className='py-3'>
+          <h2>Welcome to AddisShop</h2>
+        </Container>
+      </main>
+      <Footer />
     </div>
   );
 }
