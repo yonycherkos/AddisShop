3f41dfcb0ec4334308920277be5534348487cdaf
yonatan <yonycherkos@gmail.com>
2023-06-03 08:58:54 +0300

    003 Product List Reducer & Action

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -5,10 +5,13 @@ import {
 } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import { productListReducer } from './reducers/productReducers';
 
-const reducer = combineReducers({});
+const reducer = combineReducers({
+  productList: productListReducer,
+});
 
-const initialState = [];
+const initialState = {};
 
 const middleware = [thunk];
 
