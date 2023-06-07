fe2797702fee75dd05f3fb3aeade2afa485a2d0b
yonatan <yonycherkos@gmail.com>
2023-06-07 20:52:43 +0300

    005 Custom Authentication Middleware

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -14,10 +14,29 @@ export const authUser = asyncHandler(async (req, res) => {
       name: user.name,
       email: user.email,
       isAdmin: user.isAdmin,
-      token: generateToken(),
+      token: generateToken(user._id),
     });
   } else {
     res.status(401);
     throw new Error('Invalid email or password');
+  }
+});
+
+// @desc Get user profile
+// @route GET /api/users/profile
+// @access private
+export const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found');
   }
 });
